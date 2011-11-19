730dbd5a7b473ebdc3f36072c87161f624e27571
ryan spangler <ryan.spangler@gmail.com>
2011-11-18 21:58:59 -0800

    namespaced js

--- resources/public/js/triceratops.js
+++ resources/public/js/triceratops.js
@@ -1,61 +1,72 @@
-var socket;
-var name;
+var triceratops = function() {
+  var socket;
+  var name;
 
-function openWebSocket() {
-  if (window.WebSocket) {
-    socket = new WebSocket('ws://triceratops.me:11122');
-    socket.onopen    = function(event) { $('#alert').html('channel open!'); };
-    socket.onclose   = function(event) { $('#alert').html('channel closed'); };
-    socket.onmessage = function(event) { parse(event.data); };
-  } else {
-    alert('Your browser does not support WebSockets yet.');
+  var openWebSocket = function() {
+    if (window.WebSocket) {
+      socket = new WebSocket('ws://127.0.0.1:11122');
+      socket.onopen    = function(event) { $('#alert').html('channel open!'); };
+      socket.onclose   = function(event) { $('#alert').html('channel closed'); };
+      socket.onmessage = function(event) { parse(event.data); };
+    } else {
+      alert('Your browser does not support WebSockets yet.');
+    }
+  };
+
+  var closeWebSocket = function() {
+    socket.close();
   }
-}
 
-function init() {
-  openWebSocket();
-
-  $('#chat').hide();
-  $('#name').keypress(function(e) {
-                        if (e.which === 13) {
-                          sendName();
-                        }
-                      });
-
-  $('#voice').keypress(function(e) {
-                         if (e.which === 13) {
-                           sendVoice();
-                         }
-                       });
-}
-
-function closeWebSocket() {
-  socket.close();
-}
-
-function send(message) {
-  if (!window.WebSocket) { return; }
-  if (socket.readyState == (WebSocket.OPEN || 1)) {
-    socket.send(message);
-  } else {
-    alert('The WebSocket is not open!');
+  var send = function(message) {
+    if (!window.WebSocket) { return; }
+    if (socket.readyState == (WebSocket.OPEN || 1)) {
+      socket.send(message);
+    } else {
+      alert('The WebSocket is not open!');
+    }
   }
-}
 
-function parse( response ) {
-  $('#out').append(response+"<br/>");
-}
+  var parse = function( response ) {
+    $('#out').append(response+"<br/>");
+  }
 
-function sendName() {
-  name = $('#name').val();
-  send(name); 
-  $('title').html(name);
-  $('#pre').hide();
-  $('#chat').show();
-}
+  var sendName = function() {
+    name = $('#name').val();
+    send(name); 
+    $('title').html(name);
+    $('#pre').hide();
+    $('#chat').show();
+  }
 
-function sendVoice() {
-  send($('#voice').val()); 
-  $('#voice').val('');
-}
+  var sendVoice = function() {
+    send($('#voice').val()); 
+    $('#voice').val('');
+  }
 
+  var hatch = function() {
+    openWebSocket();
+
+    $('#chat').hide();
+    $('#name').keypress(function(e) {
+      if (e.which === 13) {
+        sendName();
+      }
+    });
+
+    $('#voice').keypress(function(e) {
+      if (e.which === 13) {
+        sendVoice();
+      }
+    });
+  }
+
+  var die = function() {
+    closeWebSocket();
+  }
+
+  return {
+    send: send,
+    hatch: hatch,
+    die: die
+  }
+}();
