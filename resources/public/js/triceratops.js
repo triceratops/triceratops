1c4e12a883be04aedd0cd9b466d13d6b8f657201
jeremy brown <jeremynealbrown@gmail.com>
2011-11-18 21:58:32 -0800

    fixed socket url

--- resources/public/js/triceratops.js
+++ resources/public/js/triceratops.js
@@ -3,7 +3,6 @@ var name;
 
 function openWebSocket() {
   if (window.WebSocket) {
-    socket = new WebSocket('ws://127.0.0.1:11111');
     socket = new WebSocket('ws://triceratops.me:11122');
     socket.onopen    = function(event) { $('#alert').html('channel open!'); };
     socket.onclose   = function(event) { $('#alert').html('channel closed'); };
