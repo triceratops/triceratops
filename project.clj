2ea3f48ed490b395c7556f21b9dd23b68969ef37
ryan spangler <ryan.spangler@gmail.com>
2011-11-18 19:27:48 -0800

    triceratops is born

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject triceratops "1.0.0-SNAPSHOT"
+  :description "collaborative realtime livecoding in the browser"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [aleph "0.2.0-rc2"]])
---
+++ test/triceratops/test/core.clj
@@ -0,0 +1,6 @@
+(ns triceratops.test.core
+  (:use [triceratops.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
